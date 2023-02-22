64b6de8ac21884875d24375b59c6fae9e54ecc0f
Raunaque97 <rpatra1997@gmail.com>
2023-02-22 09:58:52 +0530

    recursive proof WIP

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -7,6 +7,11 @@ import {
   Struct,
   Circuit,
   UInt32,
+  Experimental,
+  SelfProof,
+  MerkleMapWitness,
+  state,
+  State,
 } from 'snarkyjs';
 
 export { Board, GameOfLife };
@@ -33,72 +38,98 @@ class Board extends Struct({
   }
 }
 
+const Rollup = Experimental.ZkProgram({
+  publicInput: Board,
+
+  methods: {
+    init: {
+      privateInputs: [],
+      method(board: Board) {
+        verifyValidBoard(board);
+      },
+    },
+    step: {
+      privateInputs: [SelfProof],
+      method(newBoard: Board, earlierProof: SelfProof<Board>) {
+        earlierProof.verify();
+        verifyValidBoard(newBoard);
+        verifyCorrectTransition(earlierProof.publicInput, newBoard);
+      },
+    },
+  },
+});
+
 class GameOfLife extends SmartContract {
+  @state(Field) treeRoot = State<Field>();
+
   @method init() {
     super.init();
+    this.treeRoot.set(Field.zero);
   }
   /**
    * a still Solution is a pattern that does not change over time
    * @param solutionBoard
    */
   @method submitStillSolution(solutionBoard: Board) {
-    let board = solutionBoard.value;
     // verify wheter all values are only 0 or 1
-
-    for (let i = 0; i < boardSize; i++) {
-      for (let j = 0; j < boardSize; j++) {
-        board[i][j]
-          .equals(UInt32.zero)
-          .or(board[i][j].equals(UInt32.one))
-          .assertTrue(i + ',' + j + ' has invalid value');
-      }
-    }
-
-    this.correctTransition(solutionBoard, solutionBoard);
+    verifyValidBoard(solutionBoard);
+    verifyCorrectTransition(solutionBoard, solutionBoard);
   }
+}
 
-  /**
-   * Checks whether the transition from one board to another is valid.
-   * Rules:     dead cells with 3 neighbours are born
-   *            live cells with 2 or 3 neighbours survive
-   * @param from
-   * @param to
-   */
-  correctTransition(from: Board, to: Board) {
-    for (let i = 0; i < boardSize; i++) {
-      for (let j = 0; j < boardSize; j++) {
-        let liveNeighbours = [
-          [i - 1, j - 1],
-          [i, j - 1],
-          [i + 1, j - 1],
-          [i - 1, j],
-          [i + 1, j],
-          [i - 1, j + 1],
-          [i, j + 1],
-          [i + 1, j + 1],
-        ]
-          .filter(
-            ([i, j]) => i >= 0 && i < boardSize && j >= 0 && j < boardSize
-          )
-          .map(([i, j]) => from.value[i][j])
-          .reduce((a, b) => a.add(b.toString()), Field(0));
+/**
+ * verify wheter all values are only 0 or 1
+ */
+function verifyValidBoard(board: Board) {
+  for (let i = 0; i < boardSize; i++) {
+    for (let j = 0; j < boardSize; j++) {
+      board.value[i][j]
+        .equals(UInt32.zero)
+        .or(board.value[i][j].equals(UInt32.one))
+        .assertTrue(i + ',' + j + ' has invalid value');
+    }
+  }
+}
 
-        let newValue = Circuit.if(
-          from.value[i][j].equals(UInt32.zero),
-          // if the cell is dead it is gets reborn if it has 3 neighbours
-          Circuit.if(liveNeighbours.equals(3), UInt32.one, UInt32.zero),
-          // if the cell is alive it survives if it has 2 or 3 neighbours
-          Circuit.if(
-            liveNeighbours.equals(3).or(liveNeighbours.equals(2)),
-            UInt32.one,
-            UInt32.zero
-          )
-        );
+/**
+ * Checks whether the transition from one board to another is valid.
+ * Rules:     dead cells with 3 neighbours are born
+ *            live cells with 2 or 3 neighbours survive
+ * @param from
+ * @param to
+ */
+function verifyCorrectTransition(from: Board, to: Board) {
+  for (let i = 0; i < boardSize; i++) {
+    for (let j = 0; j < boardSize; j++) {
+      let liveNeighbours = [
+        [i - 1, j - 1],
+        [i, j - 1],
+        [i + 1, j - 1],
+        [i - 1, j],
+        [i + 1, j],
+        [i - 1, j + 1],
+        [i, j + 1],
+        [i + 1, j + 1],
+      ]
+        .filter(([i, j]) => i >= 0 && i < boardSize && j >= 0 && j < boardSize)
+        .map(([i, j]) => from.value[i][j])
+        .reduce((a, b) => a.add(b.toString()), Field(0));
 
-        to.value[i][j]
-          .equals(newValue)
-          .assertTrue('transition is not correct for ' + i + ',' + j);
-      }
+      let newValue = Circuit.if(
+        from.value[i][j].equals(UInt32.zero),
+        // if the cell is dead it is gets reborn if it has 3 neighbours
+        Circuit.if(liveNeighbours.equals(3), UInt32.one, UInt32.zero),
+        // if the cell is alive it survives if it has 2 or 3 neighbours
+        Circuit.if(
+          liveNeighbours.equals(3).or(liveNeighbours.equals(2)),
+          UInt32.one,
+          UInt32.zero
+        )
+      );
+
+      to.value[i][j]
+        .equals(newValue)
+        .assertTrue('transition is not correct for ' + i + ',' + j);
     }
   }
 }
